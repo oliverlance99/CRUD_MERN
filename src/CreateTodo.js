79f66386e3810258924a3d275a84534e313647d2
oliverlance99 <74916346+oliverlance99@users.noreply.github.com>
2021-08-31 12:33:16 +0800

    Front End (Partial)

---
+++ src/CreateTodo.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+export const CreateTodo = () => {
+	const { register, handleSubmit } = useForm();
+
+	const onSubmit = handleSubmit((data) => {
+		alert(JSON.stringify(data));
+	});
+
+	return (
+		<div className="container">
+			<div className="mt-3">
+				<h3>Create Todo Item</h3>
+				<form onSubmit={onSubmit}>
+					<div className="form-group">
+						<label htmlFor="text">Text:</label>
+						<input className="form-control" {...register("Text")} type="text" id="text" />
+					</div>
+					<div className="form-group mt-3">
+						<button type="submit" className="btn btn-primary">
+							Create Todo
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>	
+	);
+};
