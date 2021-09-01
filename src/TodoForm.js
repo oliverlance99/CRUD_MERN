479482cbbc5fb1a9502b0ccc15f0d95b13c9a989
oliverlance99 <74916346+oliverlance99@users.noreply.github.com>
2021-09-02 07:13:06 +0800

    Backend Done (Still with errors)

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,25 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { useHistory } from "react-router-dom";
 
 export const TodoForm = ({ todo, onSubmit }) => {
 	const { register, handleSubmit } = useForm({
-		defaultValues: { Text: todo ? todo.text : "" },
+		defaultValues: { text: todo ? todo.text : "" },
 	});
-	const history = useHistory();
 
 	const submitHandler = handleSubmit((data) => {
 		onSubmit(data)
-		history.push("/");
 	});
 
 	return (
 		<form onSubmit={submitHandler}>
 			<div className="form-group">
-				<label htmlFor="text">Text:</label>
+				<label htmlFor="Text">Text:</label>
 				<input 
 					className="form-control" 
-					{...register("Text")} 
+					{...register("text")} 
 					type="text" 
 					id="text" 
 				/>
